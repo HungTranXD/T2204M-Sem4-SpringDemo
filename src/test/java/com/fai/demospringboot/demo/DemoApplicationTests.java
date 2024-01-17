c8837647b12c485d0c975f5b965da100cc60ec02
HungTranXD <105595047+HungTranXD@users.noreply.github.com>
2024-01-17 11:08:48 +0700

    first commit

---
+++ src/test/java/com/fai/demospringboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fai.demospringboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
